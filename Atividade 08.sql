520516af1451c5f63c820acd4e8c7d3cb80a53df
Joao Paulo <joaopaulo.stz@gmail.com>
2025-10-21 17:18:09 -0300

    Add files via upload

--- Atividade 08.sql
+++ Atividade 08.sql
@@ -1,5 +1,7 @@
 SELECT
+    -- Extrai apenas a data da coluna DATETIME para agrupamento
     DATE(PD.data_pedido) AS data_do_pedido,
+    -- Soma o valor total de todos os itens vendidos naquela data
     SUM(IP.quantidade * PR.preco) AS faturamento_diario
 FROM
     Pedido PD
